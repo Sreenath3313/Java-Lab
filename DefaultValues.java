5e70b30db38cbba7d8ace2ec06d411aa49b3878d
Sreenath3313 <244g5a3313@srit.ac.in>
2025-01-31 15:32:26 +0530

    Add files via upload

---
+++ DefaultValues.java
@@ -0,0 +1,31 @@
+public class DefaultValues {
+    public static void main(String[] args) {
+        byte byteValue = 0;
+        short shortValue = 0;
+        int intValue = 0;
+        long longValue = 0L;
+        float floatValue = 0.0f;
+        double doubleValue = 0.0;
+        boolean booleanValue = false;
+        char charValue = '\u0000';
+
+        System.out.println("Default value of byte: " + byteValue);
+        System.out.println("Default value of short: " + shortValue);
+        System.out.println("Default value of int: " + intValue);
+        System.out.println("Default value of long: " + longValue);
+        System.out.println("Default value of float: " + floatValue);
+        System.out.println("Default value of double: " + doubleValue);
+        System.out.println("Default value of boolean: " + booleanValue);
+        System.out.println("Default value of char: '" + charValue + "'");
+    }
+}
+
+Output 
+Default value of byte: 0
+Default value of short: 0
+Default value of int: 0
+Default value of long: 0
+Default value of float: 0.0
+Default value of double: 0.0
+Default value of boolean: false
+Default value of char: ' '
